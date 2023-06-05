863abe70f22b232a3fe60dcf4397ba21033e093d
Puffywonton <wontonking123@outlook.com>
2023-06-05 16:11:32 +0200

    logic working ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,56 @@
+import { useState } from 'react'
 import './App.css'
 import QuizCard from './components/QuizCard'
-// import require from './vitePluginRequire'
+import shuffleArray from './components/shuffleArray'
+import hunters from './data/hunters'
 
 function App() {
+  const [usedHunters, setUsedHunter] = useState([])
+  // const [currentHunterList, setCurrentHunterList] = useState({})
+  const [questionNumber, setQuestionNumber] = useState(1)
+  const totoShuffledArray = shuffleArray(hunters)
+
+
+  const selectionOfHunters = () => {
+    const shuffledArray = structuredClone(totoShuffledArray)
+    console.log('initial array',shuffledArray)
+    let selectedHunter
+    let found = ""
+    const otherChoicesArray = []
+    while (found != undefined) {
+        if (selectedHunter != undefined && otherChoicesArray.length < 3) {
+            otherChoicesArray.push(selectedHunter)
+        }
+        selectedHunter = shuffledArray.shift()
+        found = usedHunters.find(element => element.name === selectedHunter.name)
+    }
+    while (otherChoicesArray.length < 3) {
+        let tempHunter = shuffledArray.shift()
+        otherChoicesArray.push(tempHunter)
+    }
+    let hunterChoicesArray = shuffleArray([selectedHunter, ...otherChoicesArray])
+    return ({selectedHunter, hunterChoicesArray})
+  }
+  const [currentHunterList, setCurrentHunterList] = useState(selectionOfHunters)
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    const answerId = e.target.id
+    if (answerId === currentHunterList.selectedHunter.id) {
+      console.log('you won')
+      setUsedHunter([...usedHunters, currentHunterList.selectedHunter])
+      setQuestionNumber((prev) => prev + 1)
+      console.log(usedHunters)
+      setCurrentHunterList(selectionOfHunters)
+    } else {
+      console.log('you lose')
+    }
+  }
+  console.log(currentHunterList)
   return (
     <>
       <div className="mother">
-        <QuizCard />
+        <QuizCard data={currentHunterList} onClick={handleClick} questionNumber={questionNumber} />
       </div>
     </>
   )
--- src/components/QuizCard.jsx
+++ src/components/QuizCard.jsx
@@ -1,47 +1,29 @@
-import hunters from "../data/hunters"
+/* eslint-disable react/prop-types */
 import classes from './QuizCard.module.css'
-import shuffleArray from "./shuffleArray"
 
-
-//1. import all hunters (i need to add more hunters)
-
-
-//2. randomly select 4 (my current data list gives me four)
-const selectionOfHunters = () => {
-    const shuffledArray = shuffleArray(hunters)
-    const selectionArray = shuffledArray.slice(0, 4);
-    return(selectionArray)
-}
-//3. randomly pick 1 (for the correct answer)
-//4. remember the correct answer somewhere
-//      and add the name with the 3 other names for the possible answers
-const QuizCard = () => {
-    const selectionArray = selectionOfHunters()
-    const correctAnswer = selectionArray[Math.floor(Math.random()*4)]
-    console.log(correctAnswer, 'selected')
+const QuizCard = (props) => {
+    // console.log("test", props)
+    // console.log("props", props.data.hunterChoicesArray)
+    // console.log("props", props.data.selectedHunter)
     
-    const answerArray = shuffleArray(selectionArray)
-    console.log("shuffled", answerArray)
-
-    const answers = answerArray.map((answer) => {
+    const answers = props.data.hunterChoicesArray.map((answer) => {
         return (
-            <button className={classes.option} key={answer.id} id={answer.id}>{answer.name}</button>    
+            <button onClick={props.onClick} className={classes.option} key={answer.id} id={answer.id}>{answer.name}</button>    
         )
     })
-    console.log('answers', answers)
 
     return (
         <div className={classes.card}>
           <img
-            src={correctAnswer.url}
+            src={props.data.selectedHunter.url}
             className={classes.image}
             alt="A Legendary Hunter"
           />
-          <div className={classes.question}>Question #1 -- Who is this hunter?</div>
+          <div className={classes.question}>Question #{props.questionNumber} -- Who is this hunter?</div>
             <div className={classes.optionContainer}>
                 {answers}
             </div>          
-        </div>            
+        </div>           
     )
 }
 
